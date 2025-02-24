d034b777028b3cff1d7e9a02db70592323dc2da2
farag <mostafa.ibrahim@e-vastel.com>
2025-02-24 16:29:21 +0200

    create discovery service and customer service

--- discovery/src/test/java/com/mostafa/discovery/DiscoveryApplicationTests.java
+++ discovery/src/test/java/com/mostafa/discovery/DiscoveryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mostafa.config_server;
+package com.mostafa.discovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class DiscoveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
